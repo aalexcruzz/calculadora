9f2aec6c592d35225c31d95609290a6dcb126a74
aalexcruzz <alexander101997@hotmail.com>
2017-02-23 07:11:38 -0500

    Se añadio la funcion resta

--- calculadora.c
+++ calculadora.c
@@ -2,24 +2,24 @@
 #define BITS 64
 #define BITS2 64
 
-int opc,d1=0,d2=0,r1=0,c1=0;
-
+int opc,d1=0,d2=0,r1=0,c1=1;
+int i = 0, recordatorio = 0, suma[64];
 int Numbin[64];
 int Numbin2[64];
-int Numdec;
-int Numdec2;
-int contador;
-int contador2;
+int binario, binario2;
+int Numdec, Numdec2;
+int contador, contador2;
+
 
 void Sumar();
-void Restar();
-void Multi();
+//void Restar();
+//void Multi();
 int main ()
 {
 	
 		do
 		{
-			printf("Calculadora binaria a hexadecimal \n" ) ;
+			printf("Calculadora \n" ) ;
 			printf("Que operacion desea realizar: \n \n");
 			printf("1.Suma \n");
 			printf("2.Resta \n");
@@ -36,10 +36,10 @@ int main ()
 			Sumar( ) ;
 			break;
 		case 2: 
-			Restar( ) ;
+			//Restar( ) ;
 			break;
 		case 3: 
-			Multi( ) ;
+			//Multi( ) ;
 			break;
 		case 4:
 			return 0 ;
@@ -66,9 +66,10 @@ int main ()
 				}
 				for ( contador = BITS; contador >= 0; contador-- ) {
 					printf("%d", Numbin[contador]);
+					Numbin[contador]=binario;
 				}
 				
-				printf("Segundo dato: ") ;
+				printf("\nSegundo dato: ") ;
 				scanf("%d",&Numdec2 ) ;
 				for ( contador2 = 0; contador2 <= BITS2; contador2++ ) {
 				Numbin2[contador2] = Numdec2 % 2;
@@ -76,43 +77,97 @@ int main ()
 				}
 				for ( contador2 = BITS2; contador2 >= 0; contador2-- ) {
 				printf("%d", Numbin2[contador2]);
+				Numbin2[contador2]=binario2;
 				}
 				
-				r1=Numdec+Numdec2;
-				//printf("%d + %d = %d \n \n",Numdec,Numdec2,r1 ) ;
-				printf("\n Desea continuar sumando? \n"); 
-				printf("1-Si \n2-No \n \n") ;
-				printf("Elija un numero: ");
+				while ( binario!= 0 || binario2 != 0)
+
+						{
+
+							suma[i++] = binario % 10 + binario2 % 10 + recordatorio % 2;
+							recordatorio =Numbin[contador]  % 10 + binario2 % 10 + recordatorio / 2;
+							binario =binario2 / 10;
+							binario= binario2 / 10;
+
+						}
+
+						if (recordatorio != 0)
+							suma[i++] = recordatorio;
+							--i;
+
+						printf("Suma de dos numeros binarios : ");
+
+						while (i >= 0)
+
+					printf("%d", suma[i--]);
 					
-				
-			scanf("%d",&c1 );
+					//~ printf("%d", Numbin[contador]);
+		
+	
 			printf("\n");
-			} while (c1==1 );
-			}
-				void Restar()
+			} while (c1==1 );						
+			
+			
+			void Restar()
 			{
 			do
+			
 			{
+			
+			printf("Primer dato:");
+				scanf("%d",&Numdec);
+				
+				for ( contador = 0; contador <= BITS; contador++ ) {
+				Numbin[contador] = Numdec % 2;
+				Numdec = Numdec / 2;
+				}
+				for ( contador = BITS; contador >= 0; contador-- ) {
+					printf("%d", Numbin[contador]);
+					Numbin[contador]=binario;
+				}
+				
+				printf("\nSegundo dato: ") ;
+				scanf("%d",&Numdec2 ) ;
+				for ( contador2 = 0; contador2 <= BITS2; contador2++ ) {
+				Numbin2[contador2] = Numdec2 % 2;
+				Numdec2 = Numdec2/ 2;
+				}
+				for ( contador2 = BITS2; contador2 >= 0; contador2-- ) {
+				printf("%d", Numbin2[contador2]);
+				Numbin2[contador2]=binario2;
+				}
+				
+				while ( binario!= 0 || binario2 != 0)
+
+						{
+
+							suma[i++] = binario % 10 + binario2 % 10 + recordatorio % 2;
+							recordatorio =Numbin[contador]  % 10 + binario2 % 10 + recordatorio / 2;
+							binario =binario2 / 10;
+							binario= binario2 / 10;
+
+						}
+
+						if (recordatorio != 0)
+							suma[i++] = recordatorio;
+							--i;
+
+						printf("Resta de dos numeros binarios : ");
+
+						while (i >= 0)
+
+					printf("%d", suma[i--]);
+					
+					//~ printf("%d", Numbin[contador]);
 				
 
 			scanf("%d",&c1 );
 			printf("\n");
 			} while (c1==1 ) ;
 			}
-				void Multi()
-			{
-			do
-			{
-				
-			scanf("%d",&c1 ) ;
-			printf("\n");
-			} while (c1==1 ) ;
-
-
-
-
-
-
+			
+			
+			
 			} 
 
 
